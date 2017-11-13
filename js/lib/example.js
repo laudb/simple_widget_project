c53647af0e65ddc73834ee39ee2585cf7f9ba867
Laud Bentil <laudbentil@gmail.com>
2017-11-13 00:00:41 +0000

    add ListData model & view

--- js/lib/example.js
+++ js/lib/example.js
@@ -42,8 +42,33 @@ var HelloView = widgets.DOMWidgetView.extend({
     }
 });
 
+var ListDataModel = widgets.DOMWidgetModel.extend({
+  defaults:_.extend(widgets.DOMWidgetModel.prototype.defaults(), {
+    _model_name: 'ListDataModel',
+    _view_name: 'ListDataView',
+    _model_module: 'simple_widget',
+    _view_module: 'simple_widget',
+    _model_module_version: '0.1.0',
+    _view_module_version: '0.1.0',
+    value: 'List this data'
+  })
+});
+
+var ListDataView = widgets.DOMWidgetView.extend({
+    render: function () {
+      this.value_changed();
+      this.model.on('change:value', this.value_changed, this);
+    },
+
+    value_changed: function () {
+      this.el.textContent = this.model.get('value')
+    }
+});
+
 
 module.exports = {
     HelloModel : HelloModel,
-    HelloView : HelloView
+    HelloView : HelloView,
+    ListDataModel: ListDataModel,
+    ListDataView: ListDataView
 };
--- simple_widget/example.py
+++ simple_widget/example.py
@@ -1,5 +1,7 @@
 import ipywidgets as widgets
 from traitlets import Unicode
+from ipywidgets import Layout, Button, Box
+
 
 @widgets.register
 class HelloWorld(widgets.DOMWidget):
@@ -11,3 +13,15 @@ class HelloWorld(widgets.DOMWidget):
     _view_module_version = Unicode('^0.1.0').tag(sync=True)
     _model_module_version = Unicode('^0.1.0').tag(sync=True)
     value = Unicode('Hello World!').tag(sync=True)
+
+
+
+class ListData(widgets.DOMWidget):
+    """List Data in a column"""
+    _view_name = Unicode('ListDataView').tag(sync=True)
+    _model_name = Unicode('ListDataModel').tag(sync=True)
+    _view_module = Unicode('simple_widget').tag(sync=True)
+    _model_module = Unicode('simple_widget').tag(sync=True)
+    _view_module_version = Unicode('^0.1.0').tag(sync=True)
+    _model_module_version = Unicode('^0.1.0').tag(sync=True)
+    value = Unicode('List Some Data').tag(sync=True)
